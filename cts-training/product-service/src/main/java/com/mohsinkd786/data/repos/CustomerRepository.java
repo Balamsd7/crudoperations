d0d43603dd1e3f7276151561ca8e892554062a99
Balamsd7 <89965621+Balamsd7@users.noreply.github.com>
2021-09-02 17:07:08 +0530

    Delete cts-training directory

--- cts-training/product-service/src/main/java/com/mohsinkd786/data/repos/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mohsinkd786.data.repos;
-
-
-import com.mohsinkd786.data.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-}
