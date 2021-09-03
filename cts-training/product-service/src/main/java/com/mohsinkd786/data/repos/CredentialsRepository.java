81095304568a7889fd50bb1f9ad6a75870bc0f11
Balamsd7 <89965621+Balamsd7@users.noreply.github.com>
2021-09-03 15:50:36 +0530

    Delete cts-training/product-service directory

--- cts-training/product-service/src/main/java/com/mohsinkd786/data/repos/CredentialsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mohsinkd786.data.repos;
-
-import com.mohsinkd786.data.entities.Credentials;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CredentialsRepository extends JpaRepository<Credentials,Integer> {
-}
