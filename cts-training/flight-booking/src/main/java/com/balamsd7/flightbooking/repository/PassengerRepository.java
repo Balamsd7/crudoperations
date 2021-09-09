2839b28d996996abbd2436390d9c76dcbd57b8e7
Balamsd7 <89965621+Balamsd7@users.noreply.github.com>
2021-09-09 13:51:21 +0530

    Delete cts-training/flight-booking directory

--- cts-training/flight-booking/src/main/java/com/balamsd7/flightbooking/repository/PassengerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.balamsd7.flightbooking.repository;
-
-import com.balamsd7.flightbooking.model.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Integer> {
-}
--- cts-training/flight-booking/src/main/java/com/balamsd7/flightbooking/repository/UserRegisterRepository.java
+++
@@ -1,8 +0,0 @@
-package com.balamsd7.flightbooking.repository;
-
-import com.balamsd7.flightbooking.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRegisterRepository extends JpaRepository<User, Integer> {
-
-}
